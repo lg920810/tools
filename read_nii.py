7c7b7eb74badd6da0b824292e62f05c6ef71adc2
legal <lg920810@163.com>
2019-05-18 19:19:29 +0800

    read_nii.py

---
+++ read_nii.py
@@ -0,0 +1,11 @@
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = '/home/legal/Datasets/LITS/test/test-volume-0.nii'
+
+img = nib.load(path)
+img_arr = img.get_fdata()
+img_arr = img_arr[:,:,0].T
+plt.imshow(img_arr, cmap='gray')
+plt.show()
